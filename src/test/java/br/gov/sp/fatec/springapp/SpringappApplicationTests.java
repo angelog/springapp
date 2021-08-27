d1624a76327ef3cf48d3f84eabc4630ff194edf7
Gabriel Angelo <gabriel.bezerra@sc.senai.br>
2021-08-27 19:50:00 -0300

    commit inicial

---
+++ src/test/java/br/gov/sp/fatec/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
